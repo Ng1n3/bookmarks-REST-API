be1d950c629d06e1c0ace33c4d0a489d44c5c378
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-21 10:49:25 +0100

    implementing jwt for access_token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'
-import { BookmarkModule } from 'src/bookmark/bookmark.module';
-import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, ConfigModule.forRoot({
-    isGlobal: true
-  })],
+  imports: [JwtModule.register({}) ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private configServicie: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     // Check for existing email
     const existingUser = await this.prisma.user.findUnique({
@@ -20,23 +26,19 @@ export class AuthService {
     }
     //generate the password
     const hash = await argon.hash(dto.password);
-      //save the new user in the db
-      const user = await this.prisma.user.create({
-        data: {
-          email: dto.email,
-          hash,
-        },
-        // select: {
-        //   id: true,
-        //   email: true,
-        //   createdAt: true
-        // }
-      });
-      console.log(user);
-      delete user.hash;
-
-      //return the saved user
-      return user;
+    //save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      // select: {
+      //   id: true,
+      //   email: true,
+      //   createdAt: true
+      // }
+    });
+    return this.signToken(user.id, user.email);
   }
 
   async signin(dto: AuthDto) {
@@ -53,8 +55,23 @@ export class AuthService {
     //if password incorrect throw exception
     if (!pwMatch) throw new ForbiddenException('credentials Incorrect!');
 
-    //send back user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.configServicie.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
